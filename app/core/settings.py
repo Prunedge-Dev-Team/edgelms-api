998bdb3c8b696e82725eff027c174dd89725f4a2
IsaiahAim <halmitonoluwatobi@gmail.com>
2023-01-20 11:20:56 +0100

    fixed

--- app/core/settings.py
+++ app/core/settings.py
@@ -55,9 +55,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.ValidationErrorMiddleware',
-    'core.middleware.CaptureExceptionMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
